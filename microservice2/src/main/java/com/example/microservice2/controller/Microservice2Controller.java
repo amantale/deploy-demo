3b9ef45c90593f0d5d0e67de337cc5b5afb5d07e
amantale <amantale@bandwidth.com>
2023-10-29 09:25:18 +0200

    implemented new microservice

--- microservice2/src/main/java/com/example/microservice2/controller/Microservice2Controller.java
+++ microservice2/src/main/java/com/example/microservice2/controller/Microservice2Controller.java
@@ -1,13 +1,13 @@
-package com.example.microservice1.controller;
+package com.example.microservice2.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class Microservice1Controller {
+public class Microservice2Controller {
 
-    @GetMapping("/microservice1")
+    @GetMapping("/microservice2")
     public String displayHelloWorld(Model model){
         model.addAttribute("message", "Hello, World!");
         return "index";
